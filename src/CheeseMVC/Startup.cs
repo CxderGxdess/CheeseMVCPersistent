944731d7e2c8349042dc8039ed8c612b0de69d99
Jasmine Riley <jm.inc.stl@gmail.com>
2018-12-21 23:11:24 -0600

    Almost

--- src/CheeseMVC/Startup.cs
+++ src/CheeseMVC/Startup.cs
@@ -40,9 +40,6 @@ namespace CheeseMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, CheeseDbContext context)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
